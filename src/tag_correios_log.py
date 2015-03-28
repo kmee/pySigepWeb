cc0a92a1077b5d7888988cfcb8b71fa6c685e8a5
mstuttgart <michellstut@gmail.com>
2015-03-28 17:55:15 -0300

    tag_forma_de_pagamento adicionado[

--- src/tag_correios_log.py
+++ src/tag_correios_log.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from src import plp_xml_validator
 from src.tag_base import TagBase
+from tag_forma_de_pagamento import TagFormaDePagamento
 
 
 class TagCorreiosLog(TagBase):
@@ -8,9 +9,10 @@ class TagCorreiosLog(TagBase):
     _TIPO_ARQUIVO = 'Postagem'
 
     def __init__(self, versao, obj_tag_plp, obj_tag_remetente,
-                 lista_tag_objeto_pastal):
+                 lista_tag_objeto_pastal,
+                 obj_forma_de_pagamento=TagFormaDePagamento.A_FATURAR):
         self.versao = versao
-        self.forma_pagamento = ''
+        self.forma_pagamento = TagFormaDePagamento(obj_forma_de_pagamento)
         self.tagPLP = obj_tag_plp
         self.tag_remetente = obj_tag_remetente
         self.lista_objeto_postal = lista_tag_objeto_pastal
@@ -27,7 +29,7 @@ class TagCorreiosLog(TagBase):
         xml += u'<versao_arquivo>%s</versao_arquivo>' % self.versao
         xml += self.tagPLP.get_xml()
         xml += self.tag_remetente.get_xml()
-        xml += u'<forma_pagamento></forma_pagamento>'
+        xml += self.forma_pagamento.get_xml()
         for objeto_postal in self.lista_objeto_postal:
             xml += objeto_postal.get_xml()
         xml += u'</correioslog>'
