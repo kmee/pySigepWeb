9371086fec2b5e08791c7172a660008b76e871e1
mstuttgart <michellstut@gmail.com>
2015-03-28 16:22:53 -0300

    xml de tag_plp validado

--- pySIGEP-WEB/model/tag_plp.py
+++ pySIGEP-WEB/model/tag_plp.py
@@ -4,7 +4,7 @@ from tag_base import TagBase
 
 class TagPLP(TagBase):
 
-    def __init__(self, cartao_postagem, id_lpl=0, valor_global=0.00,
+    def __init__(self, cartao_postagem, id_lpl=False, valor_global=0.00,
                  mcu_unidade_postagem='', nome_unidade_postagem=''):
         self.id_lpl = id_lpl
         self.valor_global = valor_global
@@ -12,15 +12,30 @@ class TagPLP(TagBase):
         self.nome_unidade_postagem = nome_unidade_postagem
         self.cartao_postagem = cartao_postagem
 
-    def xml(self):
+    def get_xml(self):
         xml = u'<plp>'
-        xml += u'<id_plp>%s</id_plp>' % str(self.id_lpl) if self.id_lpl else ''
-        xml += u'<valor_global>%s</valor_global>' % str(self.valor_global) if\
-            self.valor_global else ''
+
+        aux = str(self.id_lpl) if self.id_lpl else ''
+        xml += u'<id_plp>%s</id_plp>' % aux
+
+        aux = str(self.valor_global) if self.valor_global else ''
+        xml += u'<valor_global>%s</valor_global>' % aux
+
         xml += u'<mcu_unidade_postagem>%s</mcu_unidade_postagem>' % \
                self.mcu_unidade_postagem
         xml += u'<nome_unidade_postagem>%s</nome_unidade_postagem>' %  \
                self.nome_unidade_postagem
         xml += u'<cartao_postagem>%s</cartao_postagem>' % self.cartao_postagem
         xml += u'</plp>'
+
+        TagPLP.validar_xml(xml)
         return xml
+
+    @staticmethod
+    def validar_xml(xml):
+        import plp_xml_validator
+
+        if plp_xml_validator.validate_xml(xml):
+            print u'XML TagPLP validado com sucesso!'
+        else:
+            print u'Validação de XML TagPLP falhou!'
