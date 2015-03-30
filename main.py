74242b04e434815ec5e5849c2feb03175609b53e
mstuttgart <michellstut@gmail.com>
2015-03-30 08:44:38 -0300

    Removido comentario no main.py

--- main.py
+++ main.py
@@ -31,10 +31,6 @@ def main():
     etiquetas = sv.solicita_etiquetas(sv_postagem.servico_postagem_id,
                                       qtd_etiquetas=3)
 
-    # etiquetas[0].etiqueta_sem_dig_verif = 'EC21325855 BR'
-    # etiquetas[1].etiqueta_sem_dig_verif = 'EC21325856 BR'
-    # etiquetas[2].etiqueta_sem_dig_verif = 'EC21325857 BR'
-
     digitos = sv.gera_digito_verificador_etiquetas(
         etiquetas, gerador=ServicoAtendeCliente.GERADOR_OFFLINE)
 
