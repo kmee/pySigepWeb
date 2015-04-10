e24320cbf9bb993435daeda5c31e582bbbec9e73
mstuttgart <michellstut@gmail.com>
2015-04-10 17:40:48 -0300

    Classe especialida para PAC41068 adicionada

--- src/tag_nacional.py
+++ src/tag_nacional.py
@@ -4,18 +4,16 @@ from src.tag_base import TagBase
 
 class TagNacional(TagBase):
 
-    def __init__(self, endereco, numero_nfe, valor_a_cobrar, serie_nfe='',
-                 codigo_usuario_postal='', centro_custo_cliente='',
-                 valor_nfe=False, natureza_nfe='', descricao_objeto=''):
+    def __init__(self, endereco):
         self.endereco = endereco
-        self.codigo_usuario_postal = codigo_usuario_postal
-        self.centro_custo_cliente = centro_custo_cliente
-        self.num_nfe = numero_nfe
-        self.serie_nfe = serie_nfe
-        self.valor_nfe = valor_nfe
-        self.natureza_nfe = natureza_nfe
-        self.descricao_objeto = descricao_objeto
-        self.valor_a_cobrar = valor_a_cobrar
+        self.codigo_usuario_postal = ''
+        self.centro_custo_cliente = ''
+        self.num_nfe = False
+        self.serie_nfe = False
+        self.valor_nfe = False
+        self.natureza_nfe = ''
+        self.descricao_objeto = ''
+        self.valor_a_cobrar = 0.00
 
     def get_xml(self):
 
@@ -32,8 +30,12 @@ class TagNacional(TagBase):
                self.codigo_usuario_postal
         xml += u'<centro_custo_cliente>%s</centro_custo_cliente>' % \
                self.centro_custo_cliente
-        xml += u'<numero_nota_fiscal>%d</numero_nota_fiscal>' % self.num_nfe
-        xml += u'<serie_nota_fiscal>%s</serie_nota_fiscal>' % self.serie_nfe
+
+        aux = str(self.num_nfe) if self.num_nfe else ''
+        xml += u'<numero_nota_fiscal>%s</numero_nota_fiscal>' % aux
+
+        aux = str(self.serie_nfe) if self.serie_nfe else ''
+        xml += u'<serie_nota_fiscal>%s</serie_nota_fiscal>' % aux
 
         aux = str(self.valor_nfe) if self.valor_nfe else ''
         xml += u'<valor_nota_fiscal>%s</valor_nota_fiscal>' % aux
@@ -42,9 +44,17 @@ class TagNacional(TagBase):
         xml += u'<descricao_objeto><![CDATA[%s]]></descricao_objeto>' % \
                self.descricao_objeto
 
-        aux = str(self.valor_a_cobrar) if self.valor_a_cobrar else ''
-        xml += u'<valor_a_cobrar>%s</valor_a_cobrar>' % aux
+        # aux = str(self.valor_a_cobrar) if self.valor_a_cobrar else ''
+        xml += u'<valor_a_cobrar>1.2%f</valor_a_cobrar>' % self.valor_a_cobrar
         xml += u'</nacional>'
 
         self._validar_xml(xml)
         return xml
+
+
+class TagNacionalPAC41068(TagNacional):
+
+    def __init__(self, endereco, numero_nfe, serie_nfe):
+        super(TagNacionalPAC41068, self).__init__(endereco)
+        self.num_nfe = numero_nfe
+        self.serie_nfe = serie_nfe
