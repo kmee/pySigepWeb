ef35d19cdae1f4f162e72421013bfb494731a316
mstuttgart <michellstut@gmail.com>
2015-04-13 10:54:34 -0300

    Ajustes classe Ambiente

--- src/ambiente.py
+++ src/ambiente.py
@@ -1,44 +1,33 @@
 # -*- coding: utf-8 -*-
-from usuario import Usuario
 
 
 class Ambiente(object):
 
-    def __init__(self, dominio, url):
-        self._dominio = dominio
-        self._url = dominio + url
-
     @property
     def url(self):
-        return self._url
-
-    @property
-    def dominio(self):
-        return self._dominio
+        pass
 
 
 class AmbienteHomologacao(Ambiente):
 
     def __init__(self):
-        super(AmbienteHomologacao, self).__init__(
-            'https://apphom.correios.com.br',
-            '/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl')
-
-        self._usuario_homologacao = Usuario('sigep', 'n5f9t8',
-                                            '34028316000103',  '08082650',
-                                            '9912208555', '0057018901')
+        super(AmbienteHomologacao, self).__init__()
 
     @property
-    def usuario_homogolacao(self):
-        return self._usuario_homologacao
+    def url(self):
+        return 'https://apphom.correios.com.br/SigepMasterJPA' \
+               '/AtendeClienteService/AtendeCliente?wsdl'
 
 
 class AmbienteProducao(Ambiente):
 
     def __init__(self):
-        super(AmbienteProducao, self).__init__(
-            'https://apps.correios.com.br',
-            '/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl')
+        super(AmbienteProducao, self).__init__()
+
+    @property
+    def url(self):
+        return 'https://apps.correios.com.br/SigepMasterJPA' \
+               '/AtendeClienteService/AtendeCliente?wsdl'
 
 
 class FabricaAmbiente(object):
