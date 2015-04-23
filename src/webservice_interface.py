96a9dd21b810f453740d90d938b2f9bdb274475c
mstuttgart <michellstut@gmail.com>
2015-04-23 15:51:07 -0300

    Ajuste no __init__ para lancar excecao personalizada.

--- src/webservice_interface.py
+++ src/webservice_interface.py
@@ -2,11 +2,16 @@
 try:
     from suds import client
     from suds import WebFault
+    from urllib2 import URLError
 except ImportError as exp:
     print exp.message
     print u'Módulo suds não instalado. ' \
           u'Instale com: sudo pip install suds'
     exit(-1)
+
+# import logging
+# logging.basicConfig(level=logging.DEBUG)
+from pysigep_exception import *
 
 
 class WebserviceInterface(object):
@@ -17,5 +22,6 @@ class WebserviceInterface(object):
         try:
             self._service = client.Client(url).service
         except client.TransportError as e:
-            print "[ERRO] Erro em __init__. %s" % e.message
-            exit(e.message)
+            # print "[ERRO] Erro em __init__. %s" % e.message
+            # exit(e.message)
+            raise ErroConexaoComServidor(e.message)
