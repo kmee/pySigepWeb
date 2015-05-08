f70eaabc1ce6fd30ac0d5734d49f8cf88dd207d1
mstuttgart <michellstut@gmail.com>
2015-05-08 14:50:56 -0300

    setup.py adicionado

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='pysigep-web',
+    version='Alpha',
+    packages=['src'],
+    url='',
+    license='',
+    author='Michell Stuttgart',
+    author_email='michell.stuttgart@kmee.com.br',
+    description='PySigepWeb is a API to work like interface for PySIGEP Webservices '
+)
