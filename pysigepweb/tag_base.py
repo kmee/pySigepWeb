9735ccd25d5e5a1d1fd5f1579d551181ac6ad64b
mstuttgart <michellstut@gmail.com>
2015-05-08 22:45:08 -0300

    Validator removido TagBase agora importa o arquivo XSD e realiza a validacao do XML da PLP.

--- pysigepweb/tag_base.py
+++ pysigepweb/tag_base.py
@@ -1,17 +1,43 @@
 # -*- coding: utf-8 -*-
+from lxml import etree
 import plp_xml_validator
+import os, sys
 
 
 class TagBase(object):
+
+    XLM_SCHEMA = None
 
     def get_xml(self):
         raise NotImplementedError(u"[WARNING]Método deve ser sobreescrito!")
 
     def _validar_xml(self, xml):
 
-        if plp_xml_validator.validate_xml(xml):
+        try:
+            tree = etree.fromstring(xml.encode('utf8'))
+
+            if self.XLM_SCHEMA is None:
+                xsd = open(sys.path[0] +
+                           '/pysigepweb/pysigepweb/data/plp_valid.xsd').read()
+                schema_tree = etree.fromstring(xsd)
+                self.XLM_SCHEMA = etree.XMLSchema(schema_tree)
+
+            self.XLM_SCHEMA.assertValid(tree)
+
             print '[INFO] XML %s validado com sucesso!' % \
                   self.__class__.__name__
-        else:
+
+        except etree.XMLSyntaxError as e:
+            print "[ERRO] Erro de parsing no xml", e
+            print u'[ERRO] Validação de XML %s falhou!' % \
+                  self.__class__.__name__
+
+        except etree.DocumentInvalid as e:
+            print '[ERRO] Erro validação XML fechaPLP', e
+            print u'[ERRO] Validação de XML %s falhou!' % \
+                  self.__class__.__name__
+
+        except AssertionError as e:
+            print "[ERRO] Documento Invalido", e
             print u'[ERRO] Validação de XML %s falhou!' % \
                   self.__class__.__name__
