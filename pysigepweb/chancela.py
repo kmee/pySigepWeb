6f763b899de9bff4e4a8887d750077defe9c556c
mstuttgart <michellstut@gmail.com>
2015-06-10 11:49:29 -0300

    Removido dict com os servicos. chancela agora e gerada pelo metodo busca_cliente

--- pysigepweb/chancela.py
+++ pysigepweb/chancela.py
@@ -22,37 +22,25 @@
 #
 ##############################################################################
 from PIL import Image, ImageDraw, ImageFont
+from StringIO import StringIO
 import io
 import base64
 import os
+
 BASE_DIR = os.path.dirname(__file__)
 
 
 class Chancela(object):
 
-    SEDEX = 1
-    SEDEX_10 = 2
-    SEDEX_HOJE = 3
-    E_SEDEX = 4
-    PAC = 5
-    CARTA = 6
-
-    _chancelas = {
-        SEDEX: os.path.join(BASE_DIR, 'data/images/sedex.png'),
-        SEDEX_10: os.path.join(BASE_DIR, 'data/images/sedex10.png'),
-        SEDEX_HOJE: os.path.join(BASE_DIR, 'data/images/sedex_hoje.png'),
-        E_SEDEX: os.path.join(BASE_DIR, 'data/images/e_sedex.png'),
-        PAC: os.path.join(BASE_DIR, 'data/images/pac.png'),
-        CARTA: os.path.join(BASE_DIR, 'data/images/carta.png'),
-    }
-
     _TTF_ARIAL = os.path.join(BASE_DIR, 'data/fonts/arial.ttf')
     _TTF_ARIAL_N = os.path.join(BASE_DIR, 'data/fonts/arial_negrito.ttf')
 
-    def __init__(self, tipo, num_contrato='000000000', ano_assinatura='0000',
-                 dr_origem='XX', dr_postagem='YY', nome_cliente='cliente'):
-        self._tipo = tipo
-        self._image_path = self._chancelas[tipo]
+    def __init__(self, base_64_str_imagem, descricao, num_contrato='000000000',
+                 ano_assinatura='0000', dr_origem='XX', dr_postagem='YY',
+                 nome_cliente='cliente'):
+
+        self._base_64_str_imagem = str(base_64_str_imagem)
+        self.descricao = str(descricao)
         self.num_contrato = num_contrato
         self.ano_assinatura = ano_assinatura
         self.dr_origem = dr_origem
@@ -60,42 +48,39 @@ class Chancela(object):
         self.nome_cliente = nome_cliente
 
     @property
-    def tipo(self):
-        return self._tipo
+    def base_64_str_imagem(self):
+        return self._base_64_str_imagem
 
-    @property
-    def image_path(self):
-        return self._image_path
-
-    def get_image_chancela(self):
+    def get_image_base64(self):
 
         if self.dr_origem != self.dr_postagem:
-            texto1 = "%s/%s - DR/%s/%s" % (self.num_contrato,
-                                           self.ano_assinatura,
-                                           self.dr_origem,
-                                           self.dr_postagem)
+            texto = "%s/%s - DR/%s/%s" % (self.num_contrato,
+                                          self.ano_assinatura,
+                                          self.dr_origem,
+                                          self.dr_postagem)
         else:
-            texto1 = "%s/%s - DR/%s" % (self.num_contrato,
-                                        self.ano_assinatura,
-                                        self.dr_origem)
-        texto2 = self.nome_cliente
-        imagem = Image.open(self._image_path)
+            texto = "%s/%s - DR/%s" % (self.num_contrato,
+                                       self.ano_assinatura,
+                                       self.dr_origem)
+
+        t = base64.decodestring(self._base_64_str_imagem)
+        imagem = Image.open(StringIO(t))
         img = imagem.convert("RGB")
         draw = ImageDraw.ImageDraw(img)
 
         font = ImageFont.truetype(Chancela._TTF_ARIAL, 8)
         draw.setfont(font)
-        tamanho_texto = draw.textsize(texto1)
+        tamanho_texto = draw.textsize(texto)
         h_position = (img.size[0] - tamanho_texto[0]) / 2
         v_position = img.size[1] / 2
-        draw.text((h_position, v_position), texto1, fill=(0, 0, 0))
+        draw.text((h_position, v_position), texto, fill=(0, 0, 0))
 
         font = ImageFont.truetype(Chancela._TTF_ARIAL_N, 11)
         draw.setfont(font)
-        tamanho_texto = draw.textsize(texto2)
+        tamanho_texto = draw.textsize(self.nome_cliente)
         h_position = (img.size[0] - tamanho_texto[0]) / 2
         v_position = img.size[1] / 2 + 8
-        draw.text((h_position, v_position), texto2, fill=(0, 0, 0))
+        draw.text((h_position, v_position), self.nome_cliente, fill=(0, 0, 0))
 
         # Converte a imagem resultante para base64
         tmp = io.BytesIO()
@@ -103,3 +88,8 @@ class Chancela(object):
         img = base64.b64encode(tmp.getvalue())
 
         return img
+
+    def save_image(self, path):
+        with open(path + 'jpg', 'wb') as f:
+            f.write(base64.decodestring(self._base_64_str_imagem))
+
